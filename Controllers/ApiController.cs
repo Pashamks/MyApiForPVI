14fce26f03d615d109d5f62449e76bd493e87f7b
pavlo <pavlo@DESKTOP-SM098C1>
2022-04-04 19:14:12 +0300

    Add project files.

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,126 @@
+﻿using lab5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabController: ControllerBase
+    {
+        List<GamesHistory> _gamesHistory;
+        List<LoginModel> _users;
+        public LabController()
+        {
+            _gamesHistory = new List<GamesHistory> { 
+                new GamesHistory { WinnerEmail="winner1@gmail.com", LoserEmail = "loser1@gmail.com",GameStartTime = new DateTime(2022, 4, 1) ,GameFinishedTime = DateTime.Now  },
+                new GamesHistory { WinnerEmail="winner2@gmail.com", LoserEmail = "loser1@gmail.com",GameStartTime = new DateTime(2022, 4, 2) ,GameFinishedTime = DateTime.Now  }};
+            _users = new List<LoginModel>();
+        }
+        /// <summary>
+        /// Login user
+        /// </summary>
+        /// <param ></param>
+        /// <returns></returns>
+        /// <response code="200">Returns success response</response>
+        // [ProducesResponseType(typeof(CommonResponse<UserClientSideData>), 200)]
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            var email_checker = new EmailAddressAttribute();
+            
+            if (loginModel.Password.Length > 8 && email_checker.IsValid(loginModel.Email) && string.IsNullOrEmpty(loginModel.Name))
+            {
+                _users.Add(loginModel);
+                return Ok(new ResponceMessage { Message = "You successfully login!", Status = true });
+            }
+
+            return BadRequest(new ResponceMessage { 
+                Message = "There are problems with your data: password must be longer than 8 symbols, email must be real and name can't be empty!",
+                Status = false});
+        }
+        /// <summary>
+        /// Get battles history
+        /// </summary>
+        /// <param ></param>
+        /// <returns></returns>
+        /// <response code="200">Returns success response</response>
+        [ProducesResponseType(typeof(List<GamesHistory>), 200)]
+        [Route("history")]
+        [HttpGet]
+        public async Task<IActionResult> GetHistory()
+        {
+            if(_gamesHistory.Count > 1)
+                return Ok(_gamesHistory);
+            return NoContent();
+        }
+        /// <summary>
+        /// Update user info
+        /// </summary>
+        /// <param ></param>
+        /// <returns></returns>
+        /// <response code="200">Returns success response</response>
+        // [ProducesResponseType(typeof(CommonResponse<UserClientSideData>), 200)]
+        [Route("update_user_info")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateUserInfo(LoginModel loginModel)
+        {
+            foreach (var item in _users)
+            {
+                if(item.Email == loginModel.Email)
+                {
+                    _users.Remove(item);
+                    _users.Add(loginModel);
+                    return Ok();
+                }
+            }
+
+            return NotFound();
+        }
+        /// <summary>
+        /// Play game
+        /// </summary>
+        /// <param ></param>
+        /// <returns></returns>
+        /// <response code="200">Returns success response</response>
+        // [ProducesResponseType(typeof(CommonResponse<UserClientSideData>), 200)]
+        [Route("play")]
+        [HttpPost]
+        public async Task<IActionResult> Play()
+        {
+            if(_users.Count > 2)
+            {
+                Random random = new Random();
+                int i = random.Next(1, _users.Count);
+                return Ok(new GameOponent { Name = _users[i].Name, Gmail = _users[i].Email});
+            }
+            return NoContent();
+        }
+        /// <summary>
+        /// Delete user
+        /// </summary>
+        /// <param ></param>
+        /// <returns></returns>
+        /// <response code="200">Returns success response</response>
+        // [ProducesResponseType(typeof(CommonResponse<UserClientSideData>), 200)]
+        [Route("delete")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAccount(UserToDelete userToDelete)
+        {
+            foreach (var item in _users)
+            {
+                if(item.Email == userToDelete.Email &&  item.Password == userToDelete.Password)
+                {
+                    _users.Remove(item);
+                    return Ok(new ResponceMessage { Message = "User successfully deleted", Status = true });
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+#if DEBUG
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "api/lab5 RestApi", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+               
+            });
+            #endif
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+           
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api/lab5 RestApi v1"));
+        }
+    }
+}
